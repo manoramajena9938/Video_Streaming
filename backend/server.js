d66b1b4f23fbe5a06102e2187607263f72a98d9d
manoramajena9938 <manoramajena523@gmail.com>
2026-04-13 10:16:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://cse23bcse34_db_user:lDzDTYG7NyZ3iDGX@cluster0.yngodbo.mongodb.net/register")
+mongoose.connect("")
 .then(() => console.log("MongoDB Atlas Connected"))
 .catch(err => console.log(err));
 
